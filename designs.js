0b313c9818107d7b23caccf107142e52e6708cb7
jjsoohoo <38161046+jjsoohoo@users.noreply.github.com>
2018-04-08 06:15:57 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,24 +3,32 @@
 
 // When size is submitted by the user, call makeGrid()
 
-var height, width;
-		
-$('#submit').click(function() {
-	height = $('#inputHeight').val();
-	width = $('#inputWeight').val();
-	makeGrid();
-});
-		
-
-function makeGrid() {
+$('#submit').click(function(event) {  // when 'submit' button is clicked
+	event.preventDefault();  // prevent form from sending data to server
 	
-	for (let i = 0; i < height; i++) {
-		$('#pixelCanvas').append($('<tr></tr>'));
-		
-		for (let j = 0; j < width; j++) {
-			$('tr').last().append($('<td></td>'));
-		};
-		
+	while ($('#pixelCanvas').children().length != 0) {  // if table is not empty...
+		$('#pixelCanvas').empty();  // clear all child elements from table
 	};
 	
+	const height = $('#inputHeight').val();  // use 'const' for global variable value
+	const width = $('#inputWeight').val();
+	
+	makeGrid(height,width);  // call function with parameters
+});
+	
+$('#clear').click(function() {  // when 'reset' button is clicked
+	$('#pixelCanvas').empty();  // clear all child elements from table
+	$('#inputHeight').val('1');  // reset input boxes to '1'
+	$('#inputWeight').val('1');
+});
+
+function makeGrid(height,width) {
+	
+	for (let i = 0; i < height; i++) {
+		$('#pixelCanvas').append($('<tr></tr>'));  // add specified number of rows
+		
+		for (let j = 0; j < width; j++) {
+			$('tr').last().append($('<td>'+ i + ',' + j + '</td>'));  // add cells to the last row
+		};
+	};
 };
